233862c76e8b0e12a93821c424a1666c5e64f8db
Enis Getmez <mentalistler@mit.tc>
2020-01-24 20:09:29 +0300

    Update vision.py

--- Color Detect/vision.py
+++ Color Detect/vision.py
@@ -9,25 +9,25 @@ import time
 import imutils
 x = 0 #programın ileride hata vermemesi için x 0 olarak tanımlıyorum
 y = 0 # programın ileride hata vermemesi için y 0 olarak tanımlıyorum
-NetworkTables.initialize(server='roborio-6025-frc.local') # Roborio ile iletişim kuruyoruz
+NetworkTables.initialize(server='roborio-8208-frc.local') # Roborio ile iletişim kuruyoruz
 table = NetworkTables.getTable("Vision") # table oluşturuyoruz
 
-
-#sari rengin algilanmasi
-colorLower = (24, 100, 100)
-colorUpper = (44, 255, 255)
+#rengin algılanması
+colorLower = (60, 255, 204)
+colorUpper = (95, 255, 251)
 #converter.py ile convert ettiğiniz rengi buraya giriniz
-camera = cv2.VideoCapture(0) # kameradan
+camera = cv2.VideoCapture(0) #  webcamin bagli oldugu port varsayilan 0
+camera.set(15, -10)
+
 while True: #yazılımımız çalıştığı sürece aşağıdaki işlemleri tekrarla
-
-
+     
      (grabbed, frame) = camera.read() # grabbed ve frame değişkenini camera.read olarak tanımlıyoruz.
 
-     frame = imutils.resize(frame, width=600) # görüntü genişliğini 600p yapıyoruz
+
+     frame = imutils.resize(frame, width=320, height=240) # görüntü genişliğini ayarlıyorum
      frame = imutils.rotate(frame, angle=0) # görüntüyü sabitliyoruz
 
      hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # görüntüyü hsv formatına çeviriyoruz
-
      mask = cv2.inRange(hsv, colorLower, colorUpper) # hsv formatına dönen görüntünün bizim belirttiğimiz renk sınırları içerisinde olanları belirliyor
      mask = cv2.erode(mask, None, iterations=2) # bizim renklerimizi işaretliyor
      mask = cv2.dilate(mask, None, iterations=2) # bizim renklerimizin genişliğini alıyor
@@ -51,13 +51,18 @@ while True: #yazılımımız çalıştığı sürece aşağıdaki işlemleri tek
 				 (0, 255, 255), 2)
 			     cv2.circle(frame, center, 5, (0, 0, 255), -1)
      else:
-        x = 0 ##değerlerin sıfırlanması
-        y = 0
+          x = 0
+          y = 0
+          r = 0
 
      print("x : ")
      print(x) # kameradan gelen görüntüde bizim rengimiz varsa x kordinatı
      print("y : ")
      print(y) # kameradan gelen görüntüde bizim rengimiz varsa y kordinatı
+     cv2.imshow("test", frame)
+     cv2.waitKey(1)
+
 
      table.putNumber("X", x) # roborioya değeri göndermek
      table.putNumber("Y", y)
+     
